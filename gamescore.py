895141ebd3e01bffc888a2a596a0f5151f2902f2
Tejaswi <tejaswisrigiri24@gmail.com>
2026-03-22 22:45:29 +0530

    modified the player score calculator

--- gamescore.py
+++ gamescore.py
@@ -1,9 +1,9 @@
-name = input("Enter the player's name: ")
+playerName = input("Enter the player's name: ")
 numOfGamesPly = int(input("Number of games played : "))
 totalScore = int(input("Total score achieved : "))
 averageScore = totalScore / numOfGamesPly
 
-print(f"\nPlayer: {name}")
+print(f"\nPlayer: {playerName}")
 print(f"\nGames Played: {numOfGamesPly}")
 print(f"\nTotal Score: {totalScore}")
 print(f"\nAverage Score: {averageScore}")
